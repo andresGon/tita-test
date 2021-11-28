ce1352bdb9c4bb3e16feee0deb7e5b992adbf643
unknown <miroenlared@gmail.com>
2021-11-28 10:49:20 -0500

    ajuste de rutas imagenes json

--- script.js
+++ script.js
@@ -2,36 +2,21 @@
 const url  = "https://raw.githubusercontent.com/andresGon/tita-test/main/images.json";
 const wrapMasonry = document.querySelector('.mansonry-wrap');
 
-/*
-fetch(url)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' + response.status);
-        return;
-      }
-      response.json().then(function(data) {
-        console.log(data);
-        
 
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-  */
-
-  async function getJson(){
+  async function fetchImagesJSON(){
     try{
       let response = await fetch(url);
       let images = await response.json();
-      console.log(images);
+     // console.log(images);
       return images;
     }catch{
       console.log('ERROR');
     }
   }
 
- getJson();
-
+  fetchImagesJSON().then(images => {
+    //console.log(images.ImagesInner);
+    images.ImagesInner.forEach(element => {
+      wrapMasonry.innerHTML += '<div class="mansonry-wImg"><img src="' + element.src +'"></div>'
+    });
+  });
