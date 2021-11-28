7a8248d31ccdb8b8c3eeb3e15bad1cc6cfc4454f
unknown <miroenlared@gmail.com>
2021-11-28 08:37:40 -0500

    ajustes json images

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
-console.log('Hola mundo!')
 
 
-const url  = "https://tita-test.netlify.app/images.json";
+const url  = "https://raw.githubusercontent.com/andresGon/tita-test/main/images.json";
 
-// conectar la data con el endpoint /todos
-async function getItems(){
-    try{
-      let res = await fetch(url);
-      return await res.json();
-    }catch{
-      console.log('error');
-    }
-  }
 
-  let items = getItems();
-  
-  console.log(items);
- 
-// funcion para imprimir toda la lista
-function printData(data){
-  data.todos.forEach(
-    function(element){
-      const todo = new TodoItem(element);
-      const html = todo.render();
-      document.querySelector(".todo-list").innerHTML += html;
+fetch("https://raw.githubusercontent.com/andresGon/tita-test/main/images.json")
+  .then((response) => {
+    return response.json()
   })
-};
+  .then((data) => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  .catch((err) => {
+    // Do something for an error here
+    console.log('error')
+  })
