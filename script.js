95ba3f6b963cbb66c26d58ad177b2117920360ce
unknown <miroenlared@gmail.com>
2021-11-28 09:37:00 -0500

    mas ajustes para el json

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-
-
+// este es el llamado del json que esta ubicado en github
 const url  = "https://raw.githubusercontent.com/andresGon/tita-test/main/images.json";
+const wrapMasonry = document.querySelector('.mansonry-wrap');
 
+async function getJson(){
+  try{
+    let response = await fetch(url);
+    let images = await response.json();
+    console.log(images);
+    return images;
+  }catch{
+    console.log(error);
+  }
+}
 
-fetch("https://raw.githubusercontent.com/andresGon/tita-test/main/images.json")
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    // Work with JSON data here
-    console.log(data)
-  })
-  .catch((err) => {
-    // Do something for an error here
-    console.log('error')
-  })
+let imgList = getJson();
