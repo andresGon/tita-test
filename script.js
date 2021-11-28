1f3c7e9161cf011ca82885fe58305858852f6596
unknown <miroenlared@gmail.com>
2021-11-28 09:47:03 -0500

    el json jode

--- script.js
+++ script.js
@@ -2,15 +2,36 @@
 const url  = "https://raw.githubusercontent.com/andresGon/tita-test/main/images.json";
 const wrapMasonry = document.querySelector('.mansonry-wrap');
 
-async function getJson(){
-  try{
-    let response = await fetch(url);
-    let images = await response.json();
-    console.log(images);
-    return images;
-  }catch{
-    console.log(error);
-  }
-}
+/*
+fetch(url)
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log(data);
+        
 
-let imgList = getJson();
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+  */
+
+  async function getJson(){
+    try{
+      let response = await fetch(url);
+      let images = await response.json();
+      console.log(images);
+      return images;
+    }catch{
+      console.log('ERROR');
+    }
+  }
+
+ getJson();
+
